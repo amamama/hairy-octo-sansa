1e401b6fe84ce9fe1c7e37316a7708a5950b349f
amamama <amtkrogzo@yahoo.co.jp>
2015-07-13 09:37:58 +0900

    Update template.js

--- template.js
+++ template.js
@@ -1,8 +1,11 @@
 function adjust_fontsize() {
-	var newStyle = document.createElement('style');newStyle.type = "text/css";
-	document.getElementsByTagName('head').item(0).appendChild(newStyle);
-	var css = document.styleSheets.item(1)
-		//追加
+	if(document.getElementsByTagName('style').length == 0) {
+		var newStyle = document.createElement('style');newStyle.type = "text/css";
+		document.getElementsByTagName('head').item(0).appendChild(newStyle);
+	} else {
+		document.styleSheets.item(1).deleteRule(0);
+	}
+	var css = document.styleSheets.item(1);
 	var idx = document.styleSheets[1].cssRules.length;
 	var winH = (window.innerHeight||document.documentElement.clientHeight||0);
 	css.insertRule("html { font-size : " + winH/20 + "px ; }", idx);
